14d3d3f9a6ef1418f0dfb5a4c65fc5785d0ac317
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-02-16 23:18:30 +0100

    Crea model, resource-controller, seeder e Requests per typologies

---
+++ database/seeders/TypologySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Typology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class TypologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
