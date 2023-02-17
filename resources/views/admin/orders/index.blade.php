a2efa47ca0c879608725910d121143832316c301
SimoVisc <simonaviscardivs@gmail.com>
2023-02-17 12:10:18 +0100

    Aggiunge index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -3,6 +3,10 @@
     @foreach ($orders as $order)
         <li>
             <h3>{{ $order->name }} </h3>
+            {{ $order->total_price }}
+            {{ $order->email }}
+            {{ $order->telephone }}
+            {{ $order->address }}
         </li>
     @endforeach
 </ul>
