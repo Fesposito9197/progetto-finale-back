f5c1a947e68be5c2fe8d7126d34c68c7682bc54e
SimoVisc <simonaviscardivs@gmail.com>
2023-02-17 11:42:18 +0100

    cambia lingua create

--- resources/orders/create.blade.php
+++ resources/orders/create.blade.php
@@ -3,16 +3,16 @@
 @section('content')
     <div class="container">
         <div class="py-4">
-            <h1>Create Order</h1>
+            <h1>Aggiungi un Ordine</h1>
             <div class="mt-4">
                 <form action="{{ route('admin.orders.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
+                        <label for="name" class="form-label">Nome</label>
                         <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
                     </div>
                     <div class="mb-3">
-                        <label for="total_price" class="form-label">Total Price</label>
+                        <label for="total_price" class="form-label">Prezzo Totale</label>
                         <input type="text" class="form-control" id="total_price" name="total_price"
                             value="{{ old('total_price') }}">
                     </div>
@@ -22,16 +22,16 @@
                             value="{{ old('email') }}">
                     </div>
                     <div class="mb-3">
-                        <label for="telephone" class="form-label">Telephone</label>
+                        <label for="telephone" class="form-label">Numero di telefono</label>
                         <input type="tel" class="form-control" id="telephone" name="telephone"
                             value="{{ old('telephone') }}">
                     </div>
                     <div class="mb-3">
-                        <label for="address" class="form-label">Address</label>
+                        <label for="address" class="form-label">Indirizzo</label>
                         <input type="text" class="form-control" id="address" name="address"
                             value="{{ old('address') }}">
                     </div>
-                    <button type="submit" class="btn btn-primary">Create</button>
+                    <button type="submit" class="btn btn-primary">Aggiungi</button>
                 </form>
             </div>
         </div>
