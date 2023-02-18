6d2fda7eac9739394d762be9280c3547a163cbba
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-02-18 11:04:48 +0100

    Aggiunge delete crud products

--- resources/views/admin/Products/index.blade.php
+++ resources/views/admin/Products/index.blade.php
@@ -21,7 +21,7 @@
                 <tbody>
                     @foreach($products as $product)
                         <tr>
-                            <th scope="row">1</th>
+                            <th scope="row">{{ $product->id }}</th>
                             <td>{{$product->name}}</td>
                             <td>{{$product->description}}</td>
                             <td>{{is_null($product->image) ? "Nessuna Immagine" : ""}}</td>
@@ -29,8 +29,32 @@
                             <td>{{$product->is_visible ? "Visibile" : "Nascosto"}}</td>
                             <td><a href="{{route('admin.products.show',$product)}}" class="btn btn-primary">Info</a></td>
                             <td><a href="{{route('admin.products.edit',$product)}}" class="btn btn-warning">Modifica</a></td>
+                            <td><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#product-modal-{{$product->id}}">Elimina</button></td>
                             <td></td>
                         </tr>
+
+                        <div class="modal fade" id="product-modal-{{$product->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Sei sicuro di voler eliminare "{{$product->name}}"?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <form action="{{route('admin.products.destroy',$product)}}"    method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                            <button type="submit" class="btn btn-primary">Conferma</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     @endforeach
                 </tbody>
             </table>
