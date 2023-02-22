32999624776c3d78ce29eea213cba75651eec6fd
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-02-22 01:54:50 +0100

    Crea seeder per order_product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,8 @@ class DatabaseSeeder extends Seeder
             TypologySeeder::class,
             Company_TypologySeeder::class,
             OrderSeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            Order_ProductSeeder::class
         ]);
     }
 }
---
+++ database/seeders/Order_ProductSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Order_ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   
+        foreach(Order::all() as $order){
+            $totalPrice=0;
+            $company=Company::inRandomOrder()->first();
+            $productsNum=count(Product::where('company_id',$company->id)->get());
+            $randCounter=rand(1,6);
+            $indexs=[];
+            for($index=0;$index<$randCounter;$index++){
+                $randId=rand(1,$productsNum);
+                if(in_array($randId,$indexs)){
+                    $index--;
+                    break;
+                }
+                array_push($indexs,$randId);
+                $product=Product::find($randId);
+                $quantity=rand(1,6);
+                $price=$product->price * $quantity;
+                $totalPrice+=$price;
+                $order->products()->attach(array_fill_keys([$randId],[
+                    'quantity'=>$quantity
+                ]));
+            }
+            $order->update(['total_price'=>$totalPrice]);
+        }
+    }
+}
