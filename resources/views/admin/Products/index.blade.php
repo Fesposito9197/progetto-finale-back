f5943d219f1d9b810d16bd38cef18bf5230dba21
Angelo31294 <luongoangelo1@gmail.com>
2023-02-23 16:20:54 +0100

    Restyling

--- resources/views/admin/Products/index.blade.php
+++ resources/views/admin/Products/index.blade.php
@@ -2,64 +2,73 @@
 
 @section('content')
         @if (count($products) > 0)
-            <h3 class="my-4">Articoli in vendita</h3>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Descrizione</th>
-                        <th scope="col">Immagine</th>
-                        <th scope="col">Prezzo</th>
-                        <th scope="col">Visibilità</th>
-                        <th scope="col">Dettagli</th>
-                        <th scope="col">Modifica</th>
-                        <th scope="col">Elimina</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
+            <div class="my-3 text-center">
+                <h3 class="my-4">Articoli in vendita</h3>
+            </div>
+            <div class="card text-bg-light m-3 p-3 shadow-sm">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <th scope="row">{{ $product->id }}</th>
-                            <td>{{ $product->name }}</td>
-                            <td>{{ $product->description }}</td>
-                            <td>{{ is_null($product->image) ? 'Nessuna Immagine' : '' }}</td>
-                            <td>{{ $product->price }}€</td>
-                            <td>{{ $product->is_visible ? 'Visibile' : 'Nascosto' }}</td>
-                            <td><a href="{{ route('admin.products.show', $product) }}" class="btn btn-primary">Info</a></td>
-                            <td><a href="{{ route('admin.products.edit', $product) }}" class="btn btn-warning">Modifica</a>
-                            </td>
-                            <td><button class="btn btn-danger" data-bs-toggle="modal"
-                                    data-bs-target="#product-modal-{{ $product->id }}">Elimina</button></td>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Descrizione</th>
+                            <th scope="col">Immagine</th>
+                            <th scope="col">Prezzo</th>
+                            <th scope="col">Visibilità</th>
+                            <th scope="col">Dettagli</th>
+                            <th scope="col">Modifica</th>
+                            <th scope="col">Elimina</th>
                         </tr>
-
-                        <div class="modal fade" id="product-modal-{{ $product->id }}" tabindex="-1"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Sei sicuro di voler eliminare "{{ $product->name }}"?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <form action="{{ route('admin.products.destroy', $product) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Annulla</button>
-                                            <button type="submit" class="btn btn-primary">Conferma</button>
-                                        </form>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                            <tr>
+                                <th scope="row">{{ $product->id }}</th>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->description }}</td>
+                                <td>{{ is_null($product->image) ? 'Nessuna Immagine' : '' }}</td>
+                                <td>{{ $product->price }}€</td>
+                                <td>{{ $product->is_visible ? 'Visibile' : 'Nascosto' }}</td>
+                                <td><a href="{{ route('admin.products.show', $product) }}" class="btn btn-primary">Info</a></td>
+                                <td><a href="{{ route('admin.products.edit', $product) }}" class="btn btn-warning">Modifica</a>
+                                </td>
+                                <td><button class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#product-modal-{{ $product->id }}">Elimina</button></td>
+                            </tr>
+    
+                            <div class="modal fade" id="product-modal-{{ $product->id }}" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Sei sicuro di voler eliminare "{{ $product->name }}"?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <form action="{{ route('admin.products.destroy', $product) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Annulla</button>
+                                                <button type="submit" class="btn btn-primary">Conferma</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
-                </tbody>
-            </table>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div>
+                    <a href="{{ route('admin.products.create') }}" class="btn btn-success">Aggiungi Articoli in Vendita</a>
+                    <a href="{{route('admin.orders.index')}}" class="btn btn-secondary mx-5 my-1">Lista Ordini</a>
+                    <a href="{{ route('admin.companies.show', $product->company_id) }}" class="btn btn-outline-primary">La tua attività</a>
+                </div>
+            </div>
         @else
             <div class="alert alert-warning mt-4">
                 <strong>
@@ -68,7 +77,5 @@
                 </strong>
             </div>
         @endif
-        <a href="{{ route('admin.products.create') }}" class="btn btn-success">Aggiungi Articoli in Vendita</a>
-        <a href="{{route('admin.orders.index')}}" class="btn btn-secondary mx-5 my-1">Lista Ordini</a>
-        <a href="{{ route('admin.companies.show', $product->company_id) }}" class="btn btn-outline-primary">La tua attività</a>
+        
 @endsection
