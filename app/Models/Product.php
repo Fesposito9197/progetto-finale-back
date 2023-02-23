e7724b0b205b702b9d2fedc60dc9eeae51448e3a
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-02-23 15:58:36 +0100

    Aggiunge imageurl nel model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,9 +13,20 @@ class Product extends Model
         "company_id",
         "slug"
     ];
+
+    protected $appends = ['image_url'];
+
+    protected function getImageUrlAttribute()
+    {   
+        if(str_starts_with($this->image , "uploads") ){
+            return $this->image ? asset("storage/$this->image") : "https://placeholder.com/assets/images/150x150-2-500x500.png";
+        }
+    }
+
     public function orders(){
         return $this->belongsToMany('App\Models\Order', 'order_product', 'product_id', 'order_id' )->withPivot('quantity');
     }
+    
     public function company(){
         return $this->belongsTo(Company::class);
     }
