f5f6d189dc39ba328927cae118e282449e67a444
Angelo31294 <luongoangelo1@gmail.com>
2023-02-24 17:30:53 +0100

    fix indentazione

--- resources/views/admin/Companies/show.blade.php
+++ resources/views/admin/Companies/show.blade.php
@@ -41,28 +41,27 @@
             </div>
         </div>
     </section>
-    
         
-        <div class="modal fade" id="company-modal-{{$company->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Sei sicuro di voler eliminare "{{$company->company_name}}"?
-                    </div>
-                    <div class="modal-footer">
-                        <form action="{{route('admin.companies.destroy',$company)}}"    method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                            <button type="submit" class="btn btn-primary">Conferma</button>
-                        </form>
-                    </div>
+    <div class="modal fade" id="company-modal-{{$company->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Sei sicuro di voler eliminare "{{$company->company_name}}"?
+                </div>
+                <div class="modal-footer">
+                    <form action="{{route('admin.companies.destroy',$company)}}"    method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                        <button type="submit" class="btn btn-primary">Conferma</button>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
