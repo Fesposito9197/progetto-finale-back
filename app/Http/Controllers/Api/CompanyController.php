4babab2ebb98e5aedfbeaa996417d95d63674a94
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-02-25 03:24:04 +0100

    Fixa la rotta Api per filtrare i ristoranti in modo che restituisca anche i prodotti

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -11,15 +11,19 @@ class CompanyController extends Controller
     public function index(Request $request)
     {
         $selectedTypologies = $request->query('typologies', []);
+
+        if (empty($selectedTypologies)) {
+            $restaurants = Company::with('typologies', 'products')->get();
+        } else {
+            $restaurants = Company::with('typologies', 'products')->whereHas('typologies', function ($query) use ($selectedTypologies) {
+                $query->whereIn('slug', $selectedTypologies);
+            })->get();
     
-        $restaurants = Company::with('typologies')->whereHas('typologies', function ($query) use ($selectedTypologies) {
-            $query->whereIn('slug', $selectedTypologies);
-        })->get();
-    
-        $restaurants = $restaurants->filter(function ($restaurant) use ($selectedTypologies) {
-            $restaurantTypologies = $restaurant->typologies->pluck('slug')->toArray();
-            return count(array_intersect($selectedTypologies, $restaurantTypologies)) === count($selectedTypologies);
-        });
+            $restaurants = $restaurants->filter(function ($restaurant) use ($selectedTypologies) {
+                $restaurantTypologies = $restaurant->typologies->pluck('slug')->toArray();
+                return count(array_intersect($selectedTypologies, $restaurantTypologies)) === count($selectedTypologies);
+            });
+        }
     
         return response()->json(['data' => $restaurants]);
     }
