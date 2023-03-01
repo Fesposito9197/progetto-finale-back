e27c310bf1c7dd317866ac461012e0dee7fc8c31
SimoVisc <simonaviscardivs@gmail.com>
2023-03-01 12:52:12 +0100

    modifica pagina  welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,29 @@
 @extends('layouts.app')
 @section('content-hero')
+    <div class="container-fluid mx-5 d-flex justify-content-center align-items-center">
+        <div class="row">
+            <div class="col-4">
+                <img class="ms-welcome-image" src="img/track.png" alt="">
+            </div>
+            <div class="col d-flex align-items-center">
 
-<div class="container-fluid px-0 d-flex">
-    <div class="container-fluid jumbotron-container px-0 d-flex align-items-center justify-content-center">
-
+                <div class="ms-welcome-text">
+                    <div class=" pt-3 pb-5">
+                        <h1>Iscriviti</h1>
+                        <p class="ms-welcome-p">Diventa un partner di Deliveboo</p>
+                    </div>
+                    <br>
+                    <div class=" pt-2 pb-5">
+                        <h1>Organizza tutto</h1>
+                        <p class="ms-welcome-p">Carica menù e foto.</p>
+                    </div>
+                    <br>
+                    <div>
+                        <h1>Vendi</h1>
+                        <p class="ms-welcome-p">Inizia a monitorare gli ordini nell'area riservata a te.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, sed cum recusandae esse illo eum praesentium accusantium eligendi officiis obcaecati veniam odit magnam est eos voluptatibus dicta vel! Pariatur, molestiae.</p>
-    </div>
-</div>
 @endsection
