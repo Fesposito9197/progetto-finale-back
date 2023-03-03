c60edf76b59a313b0822c24225809650cf9c2dbd
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-03 17:26:08 +0100

    Aggiunge grafico (non funzionante)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -126,6 +126,37 @@
                 </div>
             @endif
         </section>
+
+        <div class="my-4 text-center">
+            <h1>Statistiche ordini</h1>
+            <canvas id="myChart"></canvas>
+        </div>
+          
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+          
+        <script>
+            const ctx = document.getElementById('myChart');
+        
+            new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
+                }
+                }
+            }
+            });
+        </script>
+
     </div>
 </div>
 @endsection
