137769ba2091d117490c534abf4a34811cbce346
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-03-04 10:30:33 +0100

    fixa statistiche

--- database/migrations/2023_02_16_153412_create_orders_table.php
+++ database/migrations/2023_02_16_153412_create_orders_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('telephone', 20 );
             $table->string('address');
-            $table->string('date')->nullable;
+            $table->string('date')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -6,9 +6,7 @@
     <hr>
     <h2 class="mt-3 fs-1">Grafico Incassi</h2>
     <canvas id="myChart"></canvas>
-    <canvas id="lineChart"></canvas>
     <h2 class="mt-3 fs-1">Grafico Ordini</h2>
-    <canvas id="orders-chart" width="800" height="400"></canvas>
     <canvas id="lineOrder"></canvas>
 </div>
   
@@ -40,52 +38,6 @@
         }
     });
 
-    var ctx = document.getElementById('lineChart').getContext('2d');
-    var lineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: {!! json_encode($totalLabels) !!},
-            datasets: [{
-                label: 'Totale Incassi €',
-                data: {!! json_encode($totalData) !!},
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-
-
-    var ctx = document.getElementById('orders-chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: {!! json_encode($countLabels) !!},
-            datasets: [{
-                label: 'Totale Ordini',
-                data: {!! json_encode($countData) !!},
-                backgroundColor: 'rgba(54, 62, 325, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
 
     var ctx = document.getElementById('lineOrder').getContext('2d');
     var lineOrder = new Chart(ctx, {
